15561199ceabaa0e91aa3d7ce1d54e3a0c958924
Linda Allen <lindaallenx@gmail.com>
2019-03-25 23:02:05 -0400

    Add data persistence
    * add mongoose and create database
    * set up schema and compile into model
    * retrieve and show all trackers (find)
    * take data from form and add to trackers (create)

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose")
+
+mongoose.connect('mongodb://localhost:27017/tracker', { useNewUrlParser: true });  //creates tracker database inside of mongodb
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
-var tracker = [
-		{name: "Mood"},
-		{name: "Energy"},
-		{name: "Exercise"}
-	]
+//SCHEMA SETUP
+var trackerSchema = new mongoose.Schema({
+	name: String,
+	rating: Number,
+});
+
+var Tracker = mongoose.model("Tracker", trackerSchema); //compiles into model
+
+// Tracker.create( //create new database entry
+// 	{
+// 		name: "Energy"
+// 	}, function (err, tracker){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("Newly created tracker: ");
+// 			console.log(tracker);
+// 		}
+// 	});
 
 app.get("/", function(req, res){
 	res.render("home");
 });
 
 app.get("/tracker", function(req, res){
-	res.render("tracker",{tracker:tracker});
+	//get all trackers from DB
+	Tracker.find({}, function (err, allTrackers) {
+		if(err){
+			console.log(err);
+		} else {
+			res.render("tracker",{tracker:allTrackers}); //data passed in, and name given
+		}
+		
+	});
+
 });
 
 app.post("/tracker", function(req, res){
-	// get data from link and add to tracker object
-	var name = req.body.name;
+	//get data from link and add to tracker object
+	var name = req.body.name; //gets data from form
 	var newTracker = {name:name}
-	tracker.push(newTracker);
-	// redirect back to tracker page
-	res.redirect("/tracker");
+	//create a new tracker and save to DB
+	Tracker.create(newTracker, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			// redirect back to tracker page
+			res.redirect("/tracker");
+		}
+	});
 });
 
 // app.get("/tracker/new", function( req, res){
